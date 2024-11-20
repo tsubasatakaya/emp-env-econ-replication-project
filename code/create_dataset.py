4cf26801cf24be2c9604fb3a9071c88dd7630c67
T.Takaya <117486701+tsubasatakaya@users.noreply.github.com>
2024-11-20 09:34:03 +0100

    use DataPreprocessor instance

--- code/create_dataset.py
+++ code/create_dataset.py
@@ -1,10 +1,13 @@
 import os
 from pathlib import Path
 
-from preprocessing.prepare_data import *
+from preprocessing.preprocess import DataPreprocessor
 
 source_path = Path("replication_package")
 input_data_path = source_path/"Raw-Data"
 output_data_path = Path("data")
 
-extract_crime_data(input_data_path, output_data_path)
+preprocessor = DataPreprocessor(input_data_path,
+                                output_data_path,)
+
+preprocessor._extract_crime_data()
