6a27663aeb963318372463da20d4dbb5a3363d4f
T.Takaya <117486701+tsubasatakaya@users.noreply.github.com>
2024-11-24 11:18:17 +0100

    add setup.R

---
+++ code/setup.R
@@ -0,0 +1,11 @@
+packages = c("tidyverse", "estimatr", "modelsummary", "gt")
+
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
