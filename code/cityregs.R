2e94e9852404099a699c94599ae68737d674a6f4
T.Takaya <117486701+tsubasatakaya@users.noreply.github.com>
2024-11-25 22:23:15 +0100

    add model summary table

--- code/cityregs.R
+++ code/cityregs.R
@@ -10,6 +10,106 @@ calendar_cov <- c("ym", "dow", "month1", "jan1", "holiday")
 poll_cov <- c("avg_co_mean", "avg_no2_mean", "avg_ozone_mean")
 hist_temp_cov <- c("mean_TMAX_1991_2000")
 iv <- c("wind_bins_20")
+treatment <- "standardized_pm"
+
+
+fit_ols <- function(dv, treatment, cov) {
+   formula <- as.formula(
+     paste0(dv, "~", 
+            paste0(c(treatment, cov), collapse = "+")))
+   res <- feols(formula, data = city_data, vcov = vcov_NW(lag = 1, time = ~ date))
+}
+
+fit_iv <- function(dv, endo, cov, iv) {
+  formula <- as.formula(
+    paste0(dv, "~",
+           paste0(cov, collapse = "+"),
+           "|",
+           paste0(endo,
+                  "~",
+                  iv))
+  )
+  res <- feols(formula, data = city_data, vcov = vcov_NW(lag = 1, time = ~ date))
+}
+
+# OLS -------------------------------------------------------------------------
+ols_results <- list()
+
+ols_names <- c("(1)", "(2)", "(4)", "(5)")
+ols_dv <- c("ln_violent", "ln_violent", "ln_property", "ln_property")
+ols_cov <- list(c(calendar_cov), 
+                c(calendar_cov, weather_cov, hist_temp_cov),
+                c(calendar_cov),
+                c(calendar_cov, weather_cov, hist_temp_cov))
+for (i in seq_along(ols_names)) {
+  res <- fit_ols(ols_dv[i], treatment, pluck(ols_cov, i))
+  ols_results[[i]] <- res
+}
+names(ols_results) <- ols_names
+
+# IV --------------------------------------------------------------------------
+iv_results <- list()
+iv_names <- c("(3)", "(6)")
+iv_dv <- c("ln_violent", "ln_property")
+for (i in seq_along(iv_names)) {
+  res <- fit_iv(iv_dv[i], treatment, pluck(ols_cov, 2), iv)
+  iv_results[[i]] <- res
+}
+names(iv_results) <- iv_names
+
+# Combine
+results <- c(ols_results, iv_results)
+results <- results[sort(names(results))]
+
+# Generate table --------------------------------------------------------------
+cm <- c("standardized_pm" = "Standardized PM\U2081\U2080 reading",
+        "fit_standardized_pm" = "Standardized PM\U2081\U2080 reading")
+gm <- tribble(~raw,         ~clean,        ~fmt,
+              "nobs",       "Observations", 0,
+              "r.squared",  "R\U00B2",      2)
+
+f_violent <- round(fitstat(results[[3]], "ivf")[["ivf1::standardized_pm"]][["stat"]], 1)
+f_property <- round(fitstat(results[[6]], "ivf")[["ivf1::standardized_pm"]][["stat"]], 1)
+
+f_rows <- tibble(term = c("First stage F-statistic", "Calendar fixed effects",
+                          "Weather controls", "Historical mean temp"),
+                 col1 = c(NA, "X", NA, NA),
+                 col2 = c(NA, "X", "X", "X"),
+                 col3 = c(as.character(f_violent), "X", "X", "X"),
+                 col4 = c(NA, "X", NA, NA),
+                 col5 = c(NA, "X", "X", "X"),
+                 col6 = c(as.character(f_property), "X", "X", "X"))
+attr(f_rows, "position") <- c(3, 4, 5, 6)
+
+msummary(results, fmt = 4,
+         coef_map = cm, gof_map = gm,
+         add_rows = f_rows,
+         output = "gt") |> 
+  sub_missing(missing_text = "")
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
