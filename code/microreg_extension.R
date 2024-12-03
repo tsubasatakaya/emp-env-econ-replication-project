ae0b034f91baad84e98b219aec3819c4980ce4bc
T.Takaya <117486701+tsubasatakaya@users.noreply.github.com>
2024-12-03 21:26:19 +0100

    WIP make loop

--- code/microreg_extension.R
+++ code/microreg_extension.R
@@ -6,9 +6,10 @@ micro_data <- read_csv(file.path(data_path, "micro_dataset_original.csv")) |>
 
 fe_cov <- c("routeside")
 weather_cov <- c("tmax", "valuePRCP_MIDWAY", "avg_wind_speed")
+dummies <- c("side_dummy")
 treatment <- "treatment"
 dvs <- c("violent", "stand_crimes")
-vars_all <- c(fe_cov, weather_cov, treatment, dvs)
+vars_all <- c(fe_cov, weather_cov, dummies, treatment, dvs)
 
 micro_data <- micro_data |> 
   filter(insample == 1) |> 
@@ -18,41 +19,70 @@ micro_data <- micro_data |>
 
 violent_data <- micro_data |> 
   filter(violent == 1)
-property_data <- micro_data |> 
-  filter(violent == 0)
 
-route_id_v <- violent_data$route_id
-X_raw_v <- violent_data |> select(all_of(c(fe_cov, weather_cov)), side_dummy)
-D_v <- violent_data |> pull(treatment)
-Y_v <- violent_data |> pull("stand_crimes")
+route_id <- violent_data$route_id
+X_raw <- violent_data |> select(all_of(c(fe_cov, weather_cov)), dummies)
+D <- violent_data |> pull(treatment)
+Y <- violent_data |> pull("stand_crimes")
 
-cat_vars <- c("routeside")
-X_v <- dummy_cols(X_raw_v, select_columns = cat_vars,
-                  remove_first_dummy = TRUE) |> select(-all_of(cat_vars))
-X_v <- X_v |> 
-  mutate(across(contains("side_dummy"), .fns =  ~ . * tmax, .names = "{.col}_x_tmax"),
-         across(contains("side_dummy"), .fns =  ~ . * valuePRCP_MIDWAY, .names = "{.col}_x_prcp"))
+# Create a list of covariates, FE, and arguments for loop
+model_tags <- c("non_clustered", "clustered")
+cov_list <- list(
+  c(fe_cov, weather_cov),
+  c(weather_cov, dummies)
+)
 
-Y_forest <- regression_forest(X_v, Y_v, clusters = route_id_v, equalize.cluster.weights = TRUE)
-Y_hat <- predict(Y_forest)$predictions
-W_forest <- regression_forest(X_v, D_v, clusters = route_id_v, equalize.cluster.weights = TRUE)
-W_hat <- predict(W_forest)$predictions
+fe <- c("routeside", "side_dummy")
 
-forest_v <- causal_forest(X = X_v,
-                          Y = Y_v,
-                          W = D_v,
-                          Y.hat = Y_hat, W.hat = W_hat,
-                          clusters = route_id_v,
-                          equalize.cluster.weights = TRUE,
-                          )
+args_list <- list(
+  list(Y = Y,
+       W = D,
+       clusters = NULL,
+       equalize.cluster.weights = FALSE),
+  list(Y = Y,
+       W = D,
+       clusters = route_id,
+       equalize.cluster.weights = TRUE)
+)
 
-average_treatment_effect(forest_v, target.sample = "overlap")
+forest_results <- list()
+for (i in seq_along(model_tags)) {
+  X <- X_raw |> 
+    select(all_of(cov_list[[i]]))
+  if (nrow(unique(X[fe[i]])) != 2) {
+    X <- dummy_cols(X, select_columns = fe[i],
+                    remove_first_dummy = TRUE) |> select(-all_of(fe[i]))
+  }
+  dummy_colnames <- colnames(X)[str_detect(colnames(X), paste0(fe[i], "(_\\d)?"))]
+  X <- X |> 
+    mutate(across(dummy_colnames, .fns = ~ . * tmax, .names = "{.col}_x_tmax"),
+           across(dummy_colnames, .fns = ~ . * valuePRCP_MIDWAY, .names = "{.col}_x_prcp"))
+  
+  args <- args_list[[i]]
+  args <- append(args, list(X = X))
+  
+  print(paste("Fitting", model_tags[i], "model..."))
+  forest_results[[i]] <- do.call(causal_forest, args)
+}
 
-test_calibration(forest_v, vcov.type = "HC1")
+calibration_results <- list()
+for (i in seq_along(model_tags)) {
+  res <- test_calibration(forest_results[[i]], vcov.type = "HC1")
+  calibration_results[[i]] <- res
+}
 
-best_linear_projection(forest_v, X_v[, c("tmax", "valuePRCP_MIDWAY", "avg_wind_speed")],
-                       target.sample = "overlap",
-                       vcov.type = "HC1")
+linproj_res <- list()
+for (i in seq_along(model_tags)) {
+  cf <- forest_results[[i]]
+  res <- best_linear_projection(cf,
+                                cf$X.orig[, c("tmax", "valuePRCP_MIDWAY", "avg_wind_speed")],
+                                target.sample = "overlap",
+                                vcov.type = "HC1")
+}
+
+ato <- average_treatment_effect(forest_results[[2]], target.sample = "overlap")
+
+
 
 
 
