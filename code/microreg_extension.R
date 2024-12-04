edae58ef90edf075fa4a62bfd5ffd63b627b7820
T.Takaya <117486701+tsubasatakaya@users.noreply.github.com>
2024-12-04 10:07:25 +0100

    WIP generate forest summary tables

--- code/microreg_extension.R
+++ code/microreg_extension.R
@@ -34,7 +34,7 @@ Y <- violent_data |> pull("stand_crimes")
 
 # Model fitting ----------------------------------------------------------------
 # Create a list of covariates, FE, and arguments for loop
-model_tags <- c("non_clustered", "clustered")
+model_tags <- c("Non-clustered", "Clustered")
 cov_list <- list(
   c(fe_cov, weather_cov),
   c(weather_cov, dummies)
@@ -78,22 +78,35 @@ for (i in seq_along(model_tags)) {
 # common gof map for modelsummary
 gof_f <- function(x) format(round(x, 3), big.mark = ",")
 gm <- list(
-  list("raw" = "nobs", "clean" = "Observations", "fmt" = gof_f),
+  list("raw" = "nobs", "clean" = "Observations", "fmt" = gof_f)
 )
 
-
-
-# Generate table for heterogeneity calibration
-calibration_results <- list()
+# Generate table for heterogeneity calibration ("omnibus" test)
+calibration_res <- list()
 for (i in seq_along(model_tags)) {
   res <- test_calibration(forest_results[[i]], vcov.type = "HC1")
-  calibration_results[[i]] <- res
+  calibration_res[[i]] <- res
 }
+names(calibration_res) <- model_tags
+
+cm_calib <- c("mean.forest.prediction" = "Mean forest prediction",
+              "differential.forest.prediction" = "Differential forest prediction")
+
+add_rows <- tibble(term = c("Route \U00D7 side fixed effects", 
+                            "Route \U00D7 side weather interaction",
+                            "Side fixed effects", 
+                            "Side weather interaction"),
+                   col1 = c("X", "X", NA, NA),
+                   col2 = c(NA, NA, "X", "X"))
+attr(add_rows, "position") <- c(5:8)
 
 
+msummary(calibration_res, fmt = 3,
+         coef_map = cm_calib, gof_map = gm,
+         add_rows = add_rows,)
 
-cm <- c("treatment" = "Treatment (downwind)")
 
+# Generate table for ATO and CATE by covariate
 linproj_res <- list()
 for (i in seq_along(model_tags)) {
   cf <- forest_results[[i]]
@@ -103,6 +116,7 @@ for (i in seq_along(model_tags)) {
                                 vcov.type = "HC1")
   linproj_res[[i]] <- res
 }
+names(linproj_res) <- model_tags
 
 ato_res <- list()
 for (i in seq_along(model_tags)) {
@@ -118,14 +132,38 @@ for (i in seq_along(model_tags)) {
   class(mod) <- "modelsummary_list"
   ato_res[[i]] <- mod
 }
-
+names(ato_res) <- model_tags
 
 panels <- list(
   ato_res,
   linproj_res
 )
+panel_a_title <- "Average treatment effect for the overlap (ATO)"
+panel_b_title <- "Conditional average treatment effect"
+cm_cate <- c("treatment" = "Treatment (downwind)",
+             "tmax" = "Daily maximum temperature",
+             "valuePRCP_MIDWAY" = "Daily precipitation",
+             "avg_wind_speed" = "Daily average wind speed")
+add_rows <- tibble(term = c("Route \U00D7 side fixed effects", 
+                            "Route \U00D7 side weather interaction",
+                            "Side fixed effects", 
+                            "Side weather interaction"),
+                   col1 = c("X", "X", NA, NA),
+                   col2 = c(NA, NA, "X", "X"))
+attr(add_rows, "position") <- c(9:12)
 
-msummary(panels, fmt = 4, shape = "rbind")
+msummary(panels, fmt = 4, shape = "rbind",
+         coef_map = cm_cate, gof_map = gm,
+         add_rows = add_rows,
+         output = "gt") |> 
+  sub_missing(missing_text = "") |> 
+  tab_row_group(rows = 1:2, label = panel_a_title) |> 
+  tab_row_group(rows = 3:13, label = panel_b_title) |> 
+  row_group_order(groups = c(panel_a_title, panel_b_title)) |> 
+  # tab_footnote(footnote = footnote) |> 
+  tab_options(table.font.size = "10pt",
+              table.width = pct(100),
+              footnotes.font.size = "10pt")
 
 
 
